caba477f0ff4fa6d3d646ba84ca948f72491d6bd
Sedat Öztürk <sedozturk@gmail.com>
2023-01-10 16:36:52 +0300

    Add project files.

---
+++ SerialOlusturma/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+
+namespace SerialOlusturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        int[] karakter = new int[36];
+        private SerialCreated.enumapp enumapp;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+//            try
+//            {
+//var dosya= new FileStream(Application.StartupPath + "\\Lisance.dll",
+//            FileMode.Open, FileAccess.Read, FileShare.Read);
+//            if (dosya != null)
+//            {
+//                string rd = new StreamReader(dosya).ReadLine();
+//                    var md =rd. Substring(rd.Length - 29, 29);
+//                    string yd = SerialCreated. Sifre_Algoritmasi(SerialCreated.SifreOlustur());
+//           if(yd==  md)
+//                    MessageBox.Show("başarılı");
+//           else
+//                    MessageBox.Show("giremrdi");
+
+//            }
+//            }
+//            catch (Exception)
+//            {
+//            }  
+            //KarakterOlustur();
+
+            //textBox1.Text = Sifre_icin_done();
+            //txtson.Text = Sifre_Algoritmasi(textBox1.Text);
+            //Dosya_Oluştur(txtson.Text);
+        }
+
+        private void Dosya_Oluştur(string sifre)
+        {
+            if (File.Exists(Application.StartupPath+"\\Lisance.dll")) ;
+            {
+                try
+                {
+                    FileStream fs = new FileStream(Application.StartupPath + "\\Lisance.dll",
+            FileMode.CreateNew, FileAccess.Write, FileShare.None);
+                    System.IO.StreamWriter yaz = new StreamWriter(fs);
+                    Random rnd = new Random();
+                    for (int i = 0; i < 200; i++)
+                    {
+                        if (i % 5 == 0)
+                        {
+                            yaz.Write("-");
+                        }
+                        if (i == 2)
+                            yaz.Write(DateTime.Now.Day);
+                        else if (i == 9)
+                            yaz.Write(DateTime.Now.Month);
+                        else if (i == 18)
+                            yaz.Write(DateTime.Now.Year);
+                        else
+                        yaz.Write((char)karakter[rnd.Next(36)]);
+                    }
+                    yaz.Write(sifre);
+                    yaz.Close();
+
+                }
+                catch (Exception e)
+                {
+
+                    MessageBox.Show(e.Message);
+                }
+            }
+
+
+        }
+
+        private string Sifre_icin_done()
+        {
+            string SerialBoard = string.Empty;
+            //ManagementObjectSearcher mos;
+            //ManagementObjectCollection mosList = null;
+            //mos = new ManagementObjectSearcher("Select * From Win32_BaseBoard");
+            //mosList = mos.Get();
+            //foreach (ManagementObject mo in mosList)
+            //{
+            //    SerialBoard = mo["SerialNumber"].ToString();
+            //}
+            Random sayi = new Random();
+            for (int i = 0; i < 28; i++)
+            {
+                SerialBoard += sayi.Next(0, 10);
+            }
+
+            //return ConvertToHex(SerialBoard).ToUpper();
+            return SerialBoard;
+        }
+
+        private void KarakterOlustur()
+        {
+            karakter[2] = 48;
+            karakter[32] = 49;
+            karakter[25] = 50;
+            karakter[33] = 51;
+            karakter[15] = 52;
+            karakter[28] = 53;
+            karakter[20] = 54;
+            karakter[30] = 55;
+            karakter[8] = 56;
+            karakter[22] = 57;
+            karakter[10] = 65;
+            karakter[3] = 66;
+            karakter[27] = 67;
+            karakter[6] = 68;
+            karakter[14] = 69;
+            karakter[4] = 70;
+            karakter[18] = 71;
+            karakter[17] = 72;
+            karakter[35] = 73;
+            karakter[19] = 74;
+            karakter[13] = 75;
+            karakter[21] = 76;
+            karakter[9] = 77;
+            karakter[23] = 78;
+            karakter[24] = 79;
+            karakter[0] = 80;
+            karakter[26] = 81;
+            karakter[12] = 82;
+            karakter[5] = 83;
+            karakter[29] = 84;
+            karakter[7] = 85;
+            karakter[31] = 86;
+            karakter[1] = 87;
+            karakter[11] = 88;
+            karakter[34] = 89;
+            karakter[16] = 90;
+
+        }
+        public string ConvertToHex(string asciiString)
+        {
+            string hex = "";
+            foreach (char c in asciiString)
+            {
+                int tmp = c;
+                hex += (Math.Log(tmp)).ToString();
+            }
+            hex = hex.Replace(",", "");
+            return hex;
+        }
+        private string sayiyacevir(string yazi)
+        {
+            string sy = "";
+            foreach (char item in yazi)
+            {
+                int tmp = item;
+                sy += tmp.ToString();
+            }
+            return sy;
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            char[] c = textBox2.Text.ToCharArray();
+            int a = c[c.Length - 1];
+            textBox3.Text = a.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text =SerialCreated. Sifre_Algoritmasi(textBox1.Text,enumapp);
+        }
+
+        private string Sifre_Algoritmasi(string done)
+        {
+            string text = "";
+            if (done.Length>26)
+            {
+             text = done.Substring(done.Length -26, 26);
+
+            }
+            else
+            {
+                Random rn=new Random();
+                
+                for (int i= done.Length; i < 27; i++)
+                {
+                    done += rn.Next(0, 9);
+                }
+                text = done.Substring(done.Length - 26, 26);
+            }
+            string sifre = "";
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                int sayi1 = int.Parse(text.Substring(i, 1));
+                int sayi2 = int.Parse(text.Substring(i + 1, 1));
+                int sayi3 = sayiayarla(sayi1, sayi2);
+                sifre += (char)karakter[sayi3];
+            }
+
+            for (int i = 1; i < 5; i++)
+            {
+                sifre = sifre.Insert((i * 5) + i - 1, "-");
+            }
+            return sifre;
+        }
+
+        private int sayiayarla(int sayi1, int sayi2)
+        {
+            int sayi3 = 0;
+
+            if (sayi1 == 9)
+            {
+                sayi1 = 8;
+            }
+            sayi1 *= 4;
+            if (sayi2 < 10 && sayi2 > 7)
+            {
+                sayi2 = 3;
+            }
+            else if (sayi2 < 8 && sayi2 > 5)
+            {
+                sayi2 = 1;
+            }
+            else if (sayi2 < 5 && sayi2 > 2)
+            {
+                sayi2 = 2;
+            }
+            else
+            {
+                sayi2 = 0;
+            }
+            sayi3 = sayi1 + sayi2;
+
+            return sayi3;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text =SerialCreated. SifreOlustur();
+            //txtson.Text=Sifre_Algoritmasi(textBox1.Text);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            var rd = sender as RadioButton;
+            switch (rd.Tag)
+            {
+                case "i":
+                    enumapp = SerialCreated.enumapp.image;
+                    break;
+                case "d":
+                    enumapp = SerialCreated.enumapp.dizgi;
+                    break;
+                case "s":
+                    enumapp = SerialCreated.enumapp.shell;
+                    break;
+                case "e":
+                    enumapp = SerialCreated.enumapp.excel;
+                    break;
+                case "t":
+                    enumapp = SerialCreated.enumapp.trc;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ SerialOlusturma/SerialCreated.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Management;
+
+namespace SerialOlusturma
+{
+    class SerialCreated
+    {
+        public SerialCreated()
+        {
+        }
+
+
+        public static void Dosya_Oluştur(string sifre, enumapp enumapp)
+        {
+            if (!File.Exists(Application.StartupPath + "\\Lisance.dll"))
+            {
+                try
+                {
+                    int[] karakter = new int[36];
+                    KarakterOlustur(karakter, enumapp);
+                    FileStream fs = new FileStream(Application.StartupPath + "\\Lisance.dll",
+            FileMode.CreateNew, FileAccess.Write, FileShare.None);
+                    System.IO.StreamWriter yaz = new StreamWriter(fs);
+                    Random rnd = new Random();
+                    for (int i = 0; i < 200; i++)
+                    {
+                        if (i % 5 == 0)
+                        {
+                            yaz.Write("-");
+                        }
+                        yaz.Write((char)karakter[rnd.Next(36)]);
+                    }
+                    yaz.Write(sifre);
+                    yaz.Close();
+
+                }
+                catch (Exception e)
+                {
+
+                    MessageBox.Show(e.Message);
+                }
+            }
+
+
+        }
+
+        private string Sifre_icin_done()
+        {
+            string SerialBoard = "";
+            ManagementObjectSearcher mos;
+            ManagementObjectCollection mosList = null;
+            mos = new ManagementObjectSearcher("Select * From Win32_BaseBoard");
+            mosList = mos.Get();
+            foreach (ManagementObject mo in mosList)
+            {
+                SerialBoard = mo["SerialNumber"].ToString();
+            }
+            return ConvertToHex(SerialBoard).ToUpper();
+        }
+
+        public string ConvertToHex(string asciiString)
+        {
+            string hex = "";
+            foreach (char c in asciiString)
+            {
+                int tmp = c;
+                hex += (Math.Log(tmp)).ToString();
+            }
+            hex = hex.Replace(",", "").Replace(".", "");
+            return hex;
+        }
+        private string sayiyacevir(string yazi)
+        {
+            string sy = "";
+            foreach (char item in yazi)
+            {
+                int tmp = item;
+                sy += tmp.ToString();
+            }
+            return sy;
+        }
+        public static string Sifre_Algoritmasi(string done, enumapp enumapp)
+        {
+            int[] karakter = new int[36];
+            KarakterOlustur(karakter, enumapp);
+            string text = "";
+            if (done.Length > 26)
+            {
+                text = done.Substring(done.Length - 26, 26);
+
+            }
+            else
+            {
+                Random rn = new Random();
+
+                for (int i = done.Length; i < 27; i++)
+                {
+                    done += rn.Next(0, 9);
+                }
+                text = done.Substring(done.Length - 26, 26);
+            }
+            string sifre = "";
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                int sayi1 = int.Parse(text.Substring(i, 1));
+                int sayi2 = int.Parse(text.Substring(i + 1, 1));
+                int sayi3 = sayiayarla(sayi1, sayi2);
+                sifre += (char)karakter[sayi3];
+            }
+
+            for (int i = 1; i < 5; i++)
+            {
+                sifre = sifre.Insert((i * 5) + i - 1, "-");
+            }
+            return sifre;
+            int sayiayarla(int sayi1, int sayi2)
+            {
+                int sayi3 = 0;
+
+                if (sayi1 == 9)
+                {
+                    sayi1 = 8;
+                }
+                sayi1 *= 4;
+                if (sayi2 < 10 && sayi2 > 7)
+                {
+                    sayi2 = 3;
+                }
+                else if (sayi2 < 8 && sayi2 > 5)
+                {
+                    sayi2 = 1;
+                }
+                else if (sayi2 < 5 && sayi2 > 2)
+                {
+                    sayi2 = 2;
+                }
+                else
+                {
+                    sayi2 = 0;
+                }
+                sayi3 = sayi1 + sayi2;
+
+                return sayi3;
+            }
+        }
+        public enum enumapp
+        {
+            image,
+            shell,
+            dizgi,
+            excel,
+            trc
+        }
+        static void KarakterOlustur(int[] karakter, enumapp applicarion)
+        {
+            switch (applicarion)
+            {
+                case enumapp.image:
+                    karakter[5] = 48;
+                    karakter[22] = 49;
+                    karakter[2] = 50;
+                    karakter[14] = 51;
+                    karakter[32] = 52;
+                    karakter[12] = 53;
+                    karakter[7] = 54;
+                    karakter[15] = 55;
+                    karakter[16] = 56;
+                    karakter[3] = 57;
+                    karakter[1] = 65;
+                    karakter[27] = 66;
+                    karakter[25] = 67;
+                    karakter[30] = 68;
+                    karakter[19] = 69;
+                    karakter[0] = 70;
+                    karakter[23] = 71;
+                    karakter[35] = 72;
+                    karakter[29] = 73;
+                    karakter[34] = 74;
+                    karakter[20] = 75;
+                    karakter[18] = 76;
+                    karakter[11] = 77;
+                    karakter[13] = 78;
+                    karakter[31] = 79;
+                    karakter[26] = 80;
+                    karakter[4] = 81;
+                    karakter[28] = 82;
+                    karakter[6] = 83;
+                    karakter[9] = 84;
+                    karakter[8] = 85;
+                    karakter[10] = 86;
+                    karakter[21] = 87;
+                    karakter[24] = 88;
+                    karakter[33] = 89;
+                    karakter[17] = 90;
+
+                    break;
+                case enumapp.shell:
+                    karakter[2] = 48;
+                    karakter[32] = 49;
+                    karakter[25] = 50;
+                    karakter[33] = 51;
+                    karakter[15] = 52;
+                    karakter[28] = 53;
+                    karakter[20] = 54;
+                    karakter[30] = 55;
+                    karakter[8] = 56;
+                    karakter[22] = 57;
+                    karakter[10] = 65;
+                    karakter[3] = 66;
+                    karakter[27] = 67;
+                    karakter[6] = 68;
+                    karakter[14] = 69;
+                    karakter[4] = 70;
+                    karakter[18] = 71;
+                    karakter[17] = 72;
+                    karakter[35] = 73;
+                    karakter[19] = 74;
+                    karakter[13] = 75;
+                    karakter[21] = 76;
+                    karakter[9] = 77;
+                    karakter[23] = 78;
+                    karakter[24] = 79;
+                    karakter[0] = 80;
+                    karakter[26] = 81;
+                    karakter[12] = 82;
+                    karakter[5] = 83;
+                    karakter[29] = 84;
+                    karakter[7] = 85;
+                    karakter[31] = 86;
+                    karakter[1] = 87;
+                    karakter[11] = 88;
+                    karakter[34] = 89;
+                    karakter[16] = 90;
+                    break;
+                case enumapp.dizgi:
+                    karakter[33] = 48;
+                    karakter[12] = 49;
+                    karakter[22] = 50;
+                    karakter[35] = 51;
+                    karakter[7] = 52;
+                    karakter[3] = 53;
+                    karakter[1] = 54;
+                    karakter[9] = 55;
+                    karakter[16] = 56;
+                    karakter[28] = 57;
+                    karakter[31] = 65;
+                    karakter[30] = 66;
+                    karakter[23] = 67;
+                    karakter[11] = 68;
+                    karakter[2] = 69;
+                    karakter[20] = 70;
+                    karakter[10] = 71;
+                    karakter[6] = 72;
+                    karakter[0] = 73;
+                    karakter[8] = 74;
+                    karakter[26] = 75;
+                    karakter[15] = 76;
+                    karakter[34] = 77;
+                    karakter[17] = 78;
+                    karakter[21] = 79;
+                    karakter[29] = 80;
+                    karakter[5] = 81;
+                    karakter[24] = 82;
+                    karakter[32] = 83;
+                    karakter[25] = 84;
+                    karakter[4] = 85;
+                    karakter[13] = 86;
+                    karakter[27] = 87;
+                    karakter[19] = 88;
+                    karakter[14] = 89;
+                    karakter[18] = 90;
+                    break;
+                case enumapp.excel:
+                    karakter[11] = 48;
+                    karakter[22] = 49;
+                    karakter[14] = 50;
+                    karakter[1] = 51;
+                    karakter[23] = 52;
+                    karakter[34] = 53;
+                    karakter[29] = 54;
+                    karakter[28] = 55;
+                    karakter[13] = 56;
+                    karakter[5] = 57;
+                    karakter[21] = 65;
+                    karakter[27] = 66;
+                    karakter[3] = 67;
+                    karakter[16] = 68;
+                    karakter[25] = 69;
+                    karakter[19] = 70;
+                    karakter[35] = 71;
+                    karakter[0] = 72;
+                    karakter[32] = 73;
+                    karakter[24] = 74;
+                    karakter[7] = 75;
+                    karakter[20] = 76;
+                    karakter[10] = 77;
+                    karakter[33] = 78;
+                    karakter[12] = 79;
+                    karakter[17] = 80;
+                    karakter[15] = 81;
+                    karakter[18] = 82;
+                    karakter[6] = 83;
+                    karakter[8] = 84;
+                    karakter[31] = 85;
+                    karakter[26] = 86;
+                    karakter[4] = 87;
+                    karakter[30] = 88;
+                    karakter[9] = 89;
+                    karakter[2] = 90;
+                    break;
+                case enumapp.trc:
+                    karakter[6] = 48;
+                    karakter[22] = 49;
+                    karakter[29] = 50;
+                    karakter[10] = 51;
+                    karakter[8] = 52;
+                    karakter[0] = 53;
+                    karakter[14] = 54;
+                    karakter[33] = 55;
+                    karakter[5] = 56;
+                    karakter[13] = 57;
+                    karakter[30] = 65;
+                    karakter[27] = 66;
+                    karakter[4] = 67;
+                    karakter[16] = 68;
+                    karakter[25] = 69;
+                    karakter[9] = 70;
+                    karakter[3] = 71;
+                    karakter[34] = 72;
+                    karakter[32] = 73;
+                    karakter[18] = 74;
+                    karakter[31] = 75;
+                    karakter[2] = 76;
+                    karakter[1] = 77;
+                    karakter[28] = 78;
+                    karakter[17] = 79;
+                    karakter[12] = 80;
+                    karakter[15] = 81;
+                    karakter[24] = 82;
+                    karakter[11] = 83;
+                    karakter[23] = 84;
+                    karakter[7] = 85;
+                    karakter[26] = 86;
+                    karakter[35] = 87;
+                    karakter[21] = 88;
+                    karakter[19] = 89;
+                    karakter[20] = 90;
+                    break;
+                default:
+                    karakter[2] = 48;
+                    karakter[32] = 49;
+                    karakter[25] = 50;
+                    karakter[33] = 51;
+                    karakter[15] = 52;
+                    karakter[28] = 53;
+                    karakter[20] = 54;
+                    karakter[30] = 55;
+                    karakter[8] = 56;
+                    karakter[22] = 57;
+                    karakter[10] = 65;
+                    karakter[3] = 66;
+                    karakter[27] = 67;
+                    karakter[6] = 68;
+                    karakter[14] = 69;
+                    karakter[4] = 70;
+                    karakter[18] = 71;
+                    karakter[17] = 72;
+                    karakter[35] = 73;
+                    karakter[19] = 74;
+                    karakter[13] = 75;
+                    karakter[21] = 76;
+                    karakter[9] = 77;
+                    karakter[23] = 78;
+                    karakter[24] = 79;
+                    karakter[0] = 80;
+                    karakter[26] = 81;
+                    karakter[12] = 82;
+                    karakter[5] = 83;
+                    karakter[29] = 84;
+                    karakter[7] = 85;
+                    karakter[31] = 86;
+                    karakter[1] = 87;
+                    karakter[11] = 88;
+                    karakter[34] = 89;
+                    karakter[16] = 90;
+                    break;
+            }
+
+        }
+
+
+        public static string SifreOlustur()
+        {
+            var s = new SerialCreated();
+            var done = s.Sifre_icin_done();
+            return done;
+        }
+
+    }
+}
